ad0deb992d9e438e592206d8ea1d24c712809eb2
枫 <56990748+Jane-sy@users.noreply.github.com>
2021-10-19 09:08:32 +0800

    《Python编程：从入门到实践》项目实践

---
+++ alien_invasion/src/alien_invasion.py
@@ -0,0 +1,34 @@
+import pygame
+from settings import Settings
+from game_stats import GameStats
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+from button import Button
+from scoreboard import Scoreboard
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    play_button = Button(ai_settings, screen, "Play")
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    ship = Ship(ai_settings, screen)
+    bullets = Group()   #存储子弹的编组
+    aliens = Group() #存储外星人的编组
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    while True:
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb,  ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+if __name__ == "__main__":
+    run_game()
